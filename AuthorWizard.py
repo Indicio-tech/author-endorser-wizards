6f95edf10390d12ed33181bb0029624a4a243e2b
Lynn Bendixsen <lynn@indicio.tech>
2021-09-23 18:17:25 -0600

    Changed values for values to include tags for the toolbox non-secrets.  also added some comments.
    Signed-off-by: Lynn Bendixsen <lynn@indicio.tech>

--- AuthorWizard.py
+++ AuthorWizard.py
@@ -727,6 +727,7 @@ async def createCredDef(authorDid, poolHandle):
             print(err)
             print("Error while creating cred def")
         try:
+            timestamp = str(int(datetime.datetime.now(datetime.timezone.utc).timestamp()))
             tags = json.dumps({
                 "schema_id": schemaID,
                 "schema_issuer_did": schemaID.split(":")[0],
@@ -734,8 +735,9 @@ async def createCredDef(authorDid, poolHandle):
                 "schema_version": schemaJson["version"],
                 "issuer_did": authorDid,
                 "cred_def_id": credDefId,
-                "epoch": str(int(datetime.datetime.now(datetime.timezone.utc).timestamp())),
+                "epoch": timestamp,
             })
+            #this is for aca-py
             await non_secrets.add_wallet_record(walletHandle, "cred_def_sent", credDefId, credDefId, tags)
             tags = json.dumps({
                 "cred_def_id": credDefId,
@@ -743,7 +745,16 @@ async def createCredDef(authorDid, poolHandle):
                 "state": "written",
                 "author": "self",
             })
-            value = json.dumps({"attributes": schemaJson["attrNames"]})
+            value = json.dumps({
+                "attributes": schemaJson["attrNames"],
+                "cred_def_id": credDefId,
+                "schema_id": schemaID,
+                "state": "written",
+                "author": "self",
+                "created_at": timestamp,
+                "updated_at": timestamp,
+            })
+            #this is for aries toolbox
             await non_secrets.add_wallet_record(walletHandle, "cred_def", str(uuid.uuid4()), value, tags)
         except IndyError as err:
             print(err)
